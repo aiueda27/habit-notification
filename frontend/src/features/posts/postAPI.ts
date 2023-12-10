ebcb0f1f3d8e23f0e9d3985b17cf141d7cb0dd5a
aiueda27 <aiueda27@gmail.com>
2023-12-10 16:36:36 +0800

    :sparkles: feat: DestroyPost API 作成

--- frontend/src/features/posts/postAPI.ts
+++ frontend/src/features/posts/postAPI.ts
@@ -1,4 +1,8 @@
-import { PostFormData, PostState, PostsState } from './PostSlice'
+import {
+  PostDeleteData,
+  PostFormData,
+  PostsState,
+} from './PostSlice'
 
 const API_URL = 'http://localhost:3001'
 
@@ -21,6 +25,24 @@ export const createPost = async (payload: PostFormData) => {
   const post = payload.post
   return fetch(`${API_URL}/posts.json`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      post,
+    }),
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log('error :>> ', error)
+      return {} as PostsState
+    })
+}
+
+export const destroyPost = async (payload: PostDeleteData) => {
+  const post = payload.post
+  return fetch(`${API_URL}/posts/${post.post_id}.json`, {
+    method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
