ab72865806cf1610c6ca7cdb66abb961d8068e5a
aiueda27 <aiueda27@gmail.com>
2023-12-14 20:09:19 +0800

    :sparkles: feat: loginForm　追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
 import Posts from './features/posts/Posts'
+import LoginForm from './features/login/LoginForm'
+import { useAppSelector } from './app/hooks'
+import { selectUser } from './features/login/LoginSlice'
 
 function App() {
+  const login = useAppSelector(selectUser)
+
   return (
-    <div className="App">
-      <Posts />
+    <div>
+      {login.token ? (
+        <div className="App">
+          <Posts />
+        </div>
+      ) : (
+        <LoginForm />
+      )}
     </div>
   )
 }
