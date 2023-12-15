11cda788f16db603eaa8cdf9c166492f53cb8552
aiueda27 <aiueda27@gmail.com>
2023-12-15 10:07:35 +0800

    :sparkles: feat: Tokenをローカルストーレージに保存する機能追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,17 @@ import Posts from './features/posts/Posts'
 import LoginForm from './features/login/LoginForm'
 import { useAppSelector } from './app/hooks'
 import { selectUser } from './features/login/LoginSlice'
+import { useEffect, useState } from 'react'
 
 function App() {
+  // TODO: remove accesss token with hard code for testing purpose.
+  // localStorage.removeItem('accessToken')
+  const token = localStorage.getItem('accessToken')
   const login = useAppSelector(selectUser)
 
   return (
     <div>
-      {login.token ? (
+      {token || login.token ? (
         <div className="App">
           <Posts />
         </div>
