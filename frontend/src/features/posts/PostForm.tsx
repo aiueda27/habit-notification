600a22455e8f1d4ba5fd9d32de5f062415653789
aiueda27 <aiueda27@gmail.com>
2023-12-18 17:27:59 +0800

    :sparkles: feat: PostForm タイプ、 非同期処理のプロパティ修正、 UI追加

--- frontend/src/features/posts/PostForm.tsx
+++ frontend/src/features/posts/PostForm.tsx
@@ -2,6 +2,14 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { createPostAsync } from './PostSlice'
 import { AppDispatch } from '../../app/store'
+import { Button, Flex, Input, Textarea } from '@chakra-ui/react'
+
+export type FormData = {
+  post: {
+    title?: string
+    body?: string
+  }
+}
 
 function PostForm() {
   const dispatch = useDispatch<AppDispatch>()
@@ -13,7 +21,13 @@ function PostForm() {
   ) => {
     e.preventDefault()
 
-    const formData = {
+    if (!title || !body) {
+      // TODO: Add Validation
+      alert('Please make sure to fill the inputs')
+      return
+    }
+
+    const formData: FormData = {
       post: {
         title: title,
         body: body,
@@ -29,25 +43,40 @@ function PostForm() {
   }
 
   return (
-    <div>
-      <h1>PostForm</h1>
-      <form id="postForm">
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <textarea
-          name="body"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        />
-        <button type="submit" form="postForm" onClick={(e) => submitHandler(e)}>
-          Submit
-        </button>
-      </form>
-    </div>
+    <form id="postForm">
+      <Flex align="center" justify="space-between" gap={10} w="90%" mx="auto">
+        <Flex flex={1} align="center" justify="flex-start" gap={3}>
+          <Input
+            type="text"
+            name="title"
+            value={title}
+            placeholder="Write new title"
+            _placeholder={{ color: 'brand.light.gray', fontSize: 'sm' }}
+            bg="white"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Textarea
+            bg="white"
+            placeholder="Write description"
+            _placeholder={{ color: 'brand.light.gray', fontSize: 'sm' }}
+            rows={1}
+            name="body"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </Flex>
+        <Button
+          bg="blue.light"
+          color="brand.50"
+          _hover={{ bg: 'white', color: 'blue.light' }}
+          type="submit"
+          form="postForm"
+          onClick={(e) => submitHandler(e)}
+        >
+          +
+        </Button>
+      </Flex>
+    </form>
   )
 }
 
